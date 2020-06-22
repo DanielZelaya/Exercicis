157836d32efc1b22a39c54d2881d583ebe1a7c9a
DanielZelaya <topdoctorss@gmail.com>
2020-06-22 18:28:39 +0200

    Exercicis of curse

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main ()  {
+
+	// exercie 1
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+
+}
