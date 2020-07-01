1cdcc528d899ad6fb8b67f266dd0f251014b481c
DanielZelaya <66776945+DanielZelaya@users.noreply.github.com>
2020-07-01 17:29:17 +0200

    Update exercise-2.go

--- exercise-2.go
+++ exercise-2.go
@@ -162,6 +162,18 @@ func main() {
 
 	///////////////////////////////////
 	//Exercises - Ninja Level 1
+	// exercie 1
+	//x := 42
+	//y := "James Bond"
+	//z := true
+	//fmt.Println(x, y, z)
+	//fmt.Println(x)
+	//fmt.Println(y)
+	//fmt.Println(z)
+	
+	
+	
+	
 	// Exercise 2
 	// fmt.Println(x)
 	// fmt.Println(y)
