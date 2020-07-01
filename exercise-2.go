454f02f003869b2bb42f4a89a9ffe6479b0c9554
DanielZelaya <topdoctorss@gmail.com>
2020-07-01 17:26:53 +0200

    "This is my 3 exercise"

--- exercise-2.go
+++ exercise-2.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 //var x int = 42
 //var y string = "James Bond"
 //var z bool = true
@@ -20,10 +16,105 @@ import (
 
 //)
 
-func main()  {
+/*const (
+	g1  = iota +2020
+	g2
+	g3
+	g4
+)*/
+
+func main() {
+
+	//Exercises - Ninja Level 3
+
+	//Exercise 1
+	//i := 0
+	//for i <= 10000 {
+	//	fmt.Println(i)
+	//	i++
+	//}
+
+	//Exercise 2
+	//for i := 65; i <= 90; i++ {
+	//	fmt.Println(i)
+	//	//fmt.Printf("%#U\t\n%#U\n%#U\n", i, i, i)
+	//	for j := 0; j <= 3; j++ {
+	//		fmt.Printf("\t%#U\n",i)
+	//	}
+	//}
+
+	//Exercise 3
+	// var a int = 1999
+	// for a  <= 2020 {
+	// 	fmt.Println(a)
+	// 	a = a + 1
+	// }
+
+	//Exercise 4
+	//var i int = 1999
+	//for {
+	//	if (i<=2020){
+	//		fmt.Println(i)
+	//	}
+	//	i++
+	//}
+
+	//Exercise 5
+	//for i := 10; i < 100; i++{
+	//	if i % 4 == 0 {
+	//		fmt.Println(i)
+	//	}
+	//}
+
+	//Exercise 6
+	//var a int
+	//fmt.Println(`Hello tell me if are the BOSS
+	//1----->YES
+	//2----->NO`)
+	//fmt.Scan(&a)
+	//if a == 1 {
+	//	fmt.Println("You say that you no are the BOOS, you are a little boy")
+	//} else if a == 2{
+	//	fmt.Println("You say that you no are the BOOS, you are a little boy")
+	//}
+
+	//Exercise 7
+	//var a int
+	//fmt.Println(`Hello tell me if are the BOSS
+	//1----->YES
+	//2----->NO`)
+	//fmt.Scan(&a)
+	//if a == 1 {
+	//fmt.Println("You say that you no are the BOOS, you are a little boy")
+	//} else if a == 2 {
+	//fmt.Println("You say that you no are the BOOS, you are a little boy")
+	//} else if a != 1 {
+	//fmt.Println("Kid")
+	//}
+
+	//Exercise 8
+	//b := 1
+	//switch {
+	//case b <= 11: fmt.Println("10 es mas grande que 10")
+	//fallthrough
+	//case b == 10: fmt.Println("10 es exactamnete igual a 10")
+	//case b != 10: fmt.Println("B es diferente de 10")
+	//}
+
+	//Exercise 9
+	//favSport := "surfing"
+	//switch favSport {
+	//case  "skiing":
+	//fmt.Println("go to the mountains!")
+	//case "swimming":
+	//fmt.Println("go to the pool!")
+	//case "surfing":
+	//fmt.Println("go to hawaii!")
+	//case "wingsuit flying":
+	//fmt.Println("what would you like me to say at your funeral?")
+	//}
 
 	//Exercises - Ninja Level 2
-
 	//Exercise 1
 	//var number int
 	//fmt.Println("Hello, can you give me the number than you want conver")
@@ -42,7 +133,6 @@ func main()  {
 
 	//Exercise 3
 
-
 	//fmt.Println(a)
 	//fmt.Println(b)
 	//fmt.Println(c)
@@ -52,14 +142,24 @@ func main()  {
 
 	//Exercise 4
 
-	var d int
+	//var d int
 
-	fmt.Println("Tell some number")
-	fmt.Scan(&d)
-	fmt.Printf("%d\t%b\t%x",d, d, d)
-	e := d  << 1
-	fmt.Println(e)
-	fmt.Printf("%b\t%x", e, e)
+	//fmt.Println("Tell some number")
+	//fmt.Scan(&d)
+	//fmt.Printf("%d\t%b\t%x",d, d, d)
+	//e := d  << 1
+	//fmt.Println(e)
+	//fmt.Printf("%b\t%x", e, e)
+
+	//Exercise 5
+
+	//var f string
+	//f = `Hello this is my exercise "5" from this course!!!`
+	//fmt.Printf(f)
+	//fmt.Println(g1,g2,g3,g4)
+
+	//Exercise 6
+
 	///////////////////////////////////
 	//Exercises - Ninja Level 1
 	// Exercise 2
@@ -86,8 +186,5 @@ func main()  {
 	//y = int(x)
 	//fmt.Println(y)
 	//fmt.Printf("%T\n",y)
-
-
-
 
 }
